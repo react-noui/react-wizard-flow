1c7d0b8d6d85bfae2a9ed234342376f40e770da6
notwillk <44582272+notwillk@users.noreply.github.com>
2023-02-24 05:02:03 +0000

    Add tests

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { axe } from 'jest-axe';
 
 import { createWizardFlow, useWizardFlow } from 'index';
@@ -43,15 +43,41 @@ describe('#react-wizard-flow', () => {
     expect(results).toHaveNoViolations();
   });
 
-  it('should have functioning transitions', () => {
-    expect(true).toBe(true);
+  it('should have functioning transitions', async () => {
+    const { container, getByText } = render(
+      <TestWizardFlow.Provider initialStep={TestSteps.step1} steps={STEPS} />,
+    );
+    expect(getByText('Step 1')).toBeVisible();
+    fireEvent.click(getByText("To Step 2"));
+    expect(getByText('Step 2')).toBeVisible();
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 
-  it('should have a functioning close callback', () => {
-    expect(true).toBe(true);
+  it('should have a functioning close callback', async () => {
+    const handleClose = jest.fn();
+    const { container, getByText } = render(
+      <TestWizardFlow.Provider initialStep={TestSteps.step1} steps={STEPS} onClose={handleClose} />,
+    );
+    expect(getByText('Step 1')).toBeVisible();
+    expect(handleClose.mock.calls).toHaveLength(0);
+    fireEvent.click(getByText("Close"));
+    expect(getByText('Step 1')).toBeVisible();
+    expect(handleClose.mock.calls).toHaveLength(1);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 
   it('should not blow up without a close callback', () => {
-    expect(true).toBe(true);
+    expect(async () => {
+      const { container, getByText } = render(
+        <TestWizardFlow.Provider initialStep={TestSteps.step1} steps={STEPS} />,
+      );
+      expect(getByText('Step 1')).toBeVisible();
+      fireEvent.click(getByText("Close"));
+      expect(getByText('Step 1')).toBeVisible();
+      const results = await axe(container);
+      expect(results).toHaveNoViolations();
+    }).not.toThrow()
   });
 });
